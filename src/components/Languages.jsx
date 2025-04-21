504a0300cdd6b67de1bf948272a2b3c113cbf3a1
anudeep096 <aletianudeepreddy8@gmail.com>
2025-04-22 00:12:40 +0530

    change icon size

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -6,16 +6,16 @@ import { TbBrandNextjs } from 'react-icons/tb'
 const Languages = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
-            <h1 className='my-20 text-center text-4xl'>Languages</h1>
+            <h2 className='my-20 text-center text-4xl'>Languages</h2>
             <div className='flex flex-wrap items-center justify-center gap-4'>
                 <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                    <RiReactjsFill className='text-7xl text-cyan-400'/>
+                    <RiReactjsFill className='text-5xl text-cyan-400'/>
                 </div>
                 <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                    <TbBrandNextjs className='text-7xl'/>
+                    <TbBrandNextjs className='text-5xl'/>
                 </div>
                 <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                    <SiMongodb className='text-7xl text-green-500'/>
+                    <SiMongodb className='text-5xl text-green-500'/>
                 </div>
             </div>
         </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -9,25 +9,25 @@ import { TbBrandNextjs } from 'react-icons/tb'
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
-        <h1 className='my-20 text-center text-4xl'>Technologies</h1>
+        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
         <div className='flex flex-wrap items-center justify-center gap-4'>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
+                <RiReactjsLine className='text-5xl text-cyan-400'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <TbBrandNextjs className='text-7xl'/>
+                <TbBrandNextjs className='text-5xl'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <SiMongodb className='text-7xl text-green-500'/>
+                <SiMongodb className='text-5xl text-green-500'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <DiRedis className='text-7xl text-red-700'/>
+                <DiRedis className='text-5xl text-red-700'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaNodeJs className='text-7xl text-green-500'/>
+                <FaNodeJs className='text-5xl text-green-500'/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <BiLogoPostgresql className='text-7xl text-sky-700'/>
+                <BiLogoPostgresql className='text-5xl text-sky-700'/>
             </div>
         </div>
     </div>
