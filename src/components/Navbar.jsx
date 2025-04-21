883362b177820a646dbd409d77a6669ed793e785
anudeep096 <aletianudeepreddy8@gmail.com>
2025-04-21 21:55:03 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import logo from '../assets/kevinRushLogo.png';
+import {FaLinkedin} from "react-icons/fa"
+import {FaInstagram} from "react-icons/fa"
+import {FaSquareXTwitter} from "react-icons/fa6"
+import {FaGithub} from "react-icons/fa"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='mb-20 flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'> 
+        <h1 className='text-3xl mx-2 w-11 font-bold'>AR</h1>
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaInstagram/>
+        <FaSquareXTwitter/>
+      </div>
+    </nav>
   )
 }
 
