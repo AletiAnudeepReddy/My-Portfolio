9b174c378fa0b0e18cc5e51383e7f979aa27d1d5
anudeep096 <aletianudeepreddy8@gmail.com>
2025-04-23 18:52:07 +0530

    changed padding

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -21,13 +21,13 @@ const Languages = () => {
     <div className='border-b border-neutral-800 pb-20'>
             <motion.h2 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Languages</motion.h2>
             <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:1.5}} className='flex flex-wrap items-center justify-center gap-4'>
-                <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                     <RiReactjsFill className='text-5xl text-cyan-400'/>
                 </motion.div>
-                <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                     <TbBrandNextjs className='text-5xl'/>
                 </motion.div>
-                <motion.div variants={iconVarients(3)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <motion.div variants={iconVarients(3)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                     <SiMongodb className='text-5xl text-green-500'/>
                 </motion.div>
             </motion.div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -26,22 +26,22 @@ const Technologies = () => {
     <div className='border-b border-neutral-800 pb-24'>
         <motion.h2 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-100}} transition={{duration:1.5}} className='my-20 text-center text-4xl'>Technologies</motion.h2>
         <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:1.5}} className='flex flex-wrap items-center justify-center gap-4'>
-            <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                 <RiReactjsLine className='text-5xl text-cyan-400'/>
             </motion.div>
-            <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                 <TbBrandNextjs className='text-5xl'/>
             </motion.div>
-            <motion.div variants={iconVarients(3)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div variants={iconVarients(3)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                 <SiMongodb className='text-5xl text-green-500'/>
             </motion.div>
-            <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                 <DiRedis className='text-5xl text-red-700'/>
             </motion.div>
-            <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div variants={iconVarients(4)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                 <FaNodeJs className='text-5xl text-green-500'/>
             </motion.div>
-            <motion.div variants={iconVarients(3.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <motion.div variants={iconVarients(3.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-3'>
                 <BiLogoPostgresql className='text-5xl text-sky-700'/>
             </motion.div>
         </motion.div>
